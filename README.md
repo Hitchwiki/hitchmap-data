8efd0f76cc9795d8075bee25a5a2c7a7246075c2
tillwenke <till.wenke@gmx.net>
2023-12-29 18:57:33 +0100

    comment utils

--- README.md
+++ README.md
@@ -1,43 +1,42 @@
 # hitchmap_data_cleaning
+
 Relaunching the Hitchwiki map via https://github.com/bopjesvla/hitch with more meaningful hitchhiking spots.
 
 ### cleaning.ipynb
+
 for executing the cleaning of map spots and getting a summary.
 
 ### map.ipynb
-for observing the changes visually on a map.
+
+for observing what changed through cleaning visually on a map.
 
 ---
 
-Using the spots dump from 7th March 2023 for testing here. 
+Using the spots dump from 7th March 2023 for testing here.
 
 25000 spots overall - 2500 in Germany.
 
-Cleaning procedure ideas:
+Cleaning procedure ideas in the following sequence:
 
+**Feature-based merging:**
 
-Feature-based merging:
+Merge all spots within 30 meters of a motorway service station.
 
-1. Merge all spots within 30 meters of motorway service stations.
-2. Merge all spots within 30 meters of gas stations - if not part of a service station.
-3. Merge all spots within 30 meters of parking lot - if not part of a service station or gas station.
+1. Merge all spots within 30 meters of a gas station - if not part of a service station.
+3. Merge all spots within 30 meters of a parking lot - if not part of a service station or gas station.
 
 -> move the merged spot to the center of the map feature or the node (for some gas stations)
 
-Proximity-based merging:
+[sequence is though of as where do people tend to hh: service > gas > lot]
 
-4. Merge spots that are super close to each other (consecutively located less than 50 meters away from each other) to their centroid.
+**Proximity-based merging:**
 
-Road-based merging:
+4. Merge spots that are super close to each other (consecutively located less than 50 meters away from each other) to the centroid of all those spots. e.g. 5 spots in a line each 50 m away from each other would be merged to the 3rd spot.
 
-5. Identifiy spots in areas where one doesnt need a road (see feature-based merging & port areas).
+**Road-based merging:**
+
+5. Identifiy spots in areas where one does not need a road (see feature-based merging areas & port areas) and exclude them from these steps.
 6. Identifiy closest road segment (the road you are probably waiting at) for all spots but 1-3 & 5.
 7. Delete spots that have the closest road more than 100 meters away.
-8. Merge spots that are related to the same road segment.
+8. Merge spots that are related to the same road segment. There are road segments in OSM that are x meters long at max, never cross another road and each direction of a road has its own segment. So one can safely assume that there are never multiple spots within the same segment with different hitchability.
 9. For spots within 30 and 100 meters from their road segment - move them to 30 meters away from the segment.
-
-
-
-
-
-
---
+++ utils.py
@@ -0,0 +1,169 @@
+# TODO merge this to "hitch" repo
+
+import numpy as np
+from scipy.cluster.hierarchy import dendrogram
+import pandas as pd
+
+def haversine(lon1, lat1, lon2, lat2, earth_radius=6367):
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = np.sin(dlat / 2.0) ** 2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2.0) ** 2
+
+    c = 2 * np.arcsin(np.sqrt(a))
+    km = earth_radius * c
+    return km
+
+# can handle np arrays
+# from https://stackoverflow.com/questions/29545704/fast-haversine-approximation-python-pandas
+def haversine_np(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+
+    All args must be of equal length.
+    """
+
+    # angeles degree to radian
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+    return haversine(lon1, lat1, lon2, lat2)
+
+
+def haversine_lists(x, y):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+
+    All args must be of equal length.
+    """
+
+    lon1, lat1, lon2, lat2 = map(np.radians, [x[0], x[1], y[0], y[1]])
+    return haversine(lon1, lat1, lon2, lat2)
+    
+
+
+def haversine_points(A, B):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+
+    All args must be of equal length.
+    """
+
+    lon1, lat1, lon2, lat2 = map(np.radians, [A.y, A.x, B.y, B.x])
+    return haversine(lon1, lat1, lon2, lat2)
+
+def plot_dendrogram(model, **kwargs):
+    # Create linkage matrix and then plot the dendrogram
+
+    # create the counts of samples under each node
+    counts = np.zeros(model.children_.shape[0])
+    n_samples = len(model.labels_)
+    for i, merge in enumerate(model.children_):
+        current_count = 0
+        for child_idx in merge:
+            if child_idx < n_samples:
+                current_count += 1  # leaf node
+            else:
+                current_count += counts[child_idx - n_samples]
+        counts[i] = current_count
+
+    linkage_matrix = np.column_stack(
+        [model.children_, model.distances_, counts]
+    ).astype(float)
+
+    # Plot the corresponding dendrogram
+    dendrogram(linkage_matrix, **kwargs)
+
+
+# refetching :)
+def places_from_points(points):
+    # unify encodings (some points are in cp1252, should be utf-8)
+    points.loc[points.id.isin(range(1000000, 1040000)), "comment"] = (
+        points.loc[points.id.isin(range(1000000, 1040000)), "comment"]
+        .str.encode("cp1252", errors="ignore")
+        .str.decode("utf-8", errors="ignore")
+    )
+
+    points.datetime = pd.to_datetime(points.datetime)
+
+    # aggregate features into "text" feature
+    points["text"] = (
+        points["comment"]
+        + "\n\n―"
+        + points["name"].fillna("Anonymous")
+        + points.datetime.dt.strftime(", %B %Y").fillna("")
+    )
+
+    coords_in_degrees = points[["lon", "lat", "dest_lon", "dest_lat"]].values.T
+
+    # new feature - the distance covered by the ride caught at the spot
+    points["distance_from_spot"] = haversine_np(*coords_in_degrees)
+
+    # put comments/ entries for same spot together
+    groups = points.groupby(["lat", "lon"])
+
+    # create the new places dataframe
+    # we introduce it by only taking the feature we are not aggregating from the groups "country"
+    # taking the first value of each group is arbitrary
+    places = groups[["country"]].first()
+    places["rating"] = groups.rating.mean().round()
+    places["wait"] = points[~points.wait.isnull()].groupby(["lat", "lon"]).wait.mean()
+    # eventually "distance_from_spot" become an average value too
+    places["distance_from_spot"] = (
+        points[~points.distance_from_spot.isnull()]
+        .groupby(["lat", "lon"])
+        .distance_from_spot.mean()
+    )
+    places["text"] = groups.text.apply(lambda t: "\n\n".join(t.dropna()))
+
+    # group small countries together
+    # TODO why?
+    places["country_group"] = places.country.replace(["BE", "NL", "LU"], "BNL")
+    places.country_group = places.country_group.replace(["CH", "AT", "LI"], "ALP")
+    places.country_group = places.country_group.replace(
+        ["SI", "HR", "BA", "ME", "MK", "AL", "RS", "TR"], "BAL"
+    )
+    places.country_group = places.country_group.replace(["SK", "HU"], "SKHU")
+    places.country_group = places.country_group.replace("MC", "FR")
+
+    places.reset_index(inplace=True)
+    # make sure high-rated are on top
+    places.sort_values("rating", inplace=True, ascending=False)
+
+    return places
+
+
+callback = """\
+function (row) {
+    var marker;
+    var color = {1: 'red', 2: 'orange', 3: 'yellow', 4: 'lightgreen', 5: 'lightgreen'}[row[2]];
+    var opacity = {1: 0.3, 2: 0.4, 3: 0.6, 4: 0.8, 5: 0.8}[row[2]];
+    var point = new L.LatLng(row[0], row[1])
+    marker = L.circleMarker(point, {radius: 5, weight: 1 + 1 * (row[2] == 5), fillOpacity: opacity, color: 'black', fillColor: color});
+
+    marker.on('click', function(e) {
+        if ($$('.topbar.visible')) return
+
+        points = [point]
+
+        setTimeout(() => {
+            bar('.sidebar.show-spot')
+            $$('#spot-header').innerText = `${row[0].toFixed(5)}, ${row[1].toFixed(5)}`
+            $$('#spot-summary').innerText = `Rating: ${row[2].toFixed(0)}/5
+Waiting time in minutes: ${Number.isNaN(row[4]) ? '-' : row[4].toFixed(0)}
+Ride distance in km: ${Number.isNaN(row[5]) ? '-' : row[5].toFixed(0)}`
+
+            $$('#spot-text').innerText = row[3];
+            if (!row[3] && Number.isNaN(row[5])) $$('#extra-text').innerHTML = 'No comments/ride info. To hide points like this, check out the <a href=/light.html>lightweight map</a>.'
+            else $$('#extra-text').innerHTML = ''
+        },100)
+
+        L.DomEvent.stopPropagation(e)
+    })
+
+    // if(row[2] >= 4) marker.bringToFront()
+
+    return marker;
+};
+"""
