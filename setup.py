51012f6e497b475567136fb9a591487a905bc921
Till Wenke <38500891+tillwenke@users.noreply.github.com>
2024-12-22 19:28:32 +0100

    Add PyPI package for heatchmap
    Fixes #30
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/Hitchwiki/hitchmap-data/issues/30?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="heatchmap",
+    version="0.1.0",
+    author="Hitchwiki",
+    author_email="info@hitchwiki.org",
+    description="A package for hitchhiking data analysis and visualization",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Hitchwiki/hitchmap-data",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "folium",
+        "geopandas",
+        "ipykernel",
+        "ipython",
+        "matplotlib",
+        "numpy",
+        "osmnx",
+        "pandas",
+        "scikit-learn",
+        "scipy",
+        "shapely",
+        "tqdm",
+        "lingua-language-detector",
+    ],
+)
